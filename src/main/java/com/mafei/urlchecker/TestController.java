7f997f48669e04c23a9d4a9dc1fc7e0e325ad3a0
mafei <mafei.dev@gmail.com>
2021-09-12 21:19:14 +0530

    Initial commit

---
+++ src/main/java/com/mafei/urlchecker/TestController.java
@@ -0,0 +1,42 @@
+package com.mafei.urlchecker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TestController implements ApplicationListener<WebServerInitializedEvent> {
+    int aPort;
+    @Autowired
+    private HttpServletRequest request;
+
+    @GetMapping
+    public Object index() throws UnknownHostException {
+        Map<String, Object> data = new HashMap<>();
+        data.put("aPort", aPort);
+        data.put("getRequestURI", request.getRequestURI());
+        data.put("getRequestURL", request.getRequestURL());
+        data.put("getPathInfo", request.getPathInfo());
+        data.put("getHostAddress-remote", InetAddress.getLoopbackAddress().getHostAddress());
+        data.put("getHostName-remote", InetAddress.getLoopbackAddress().getHostName());
+
+
+        data.put("getHostAddress-local", InetAddress.getLocalHost().getHostAddress());
+        data.put("getHostName-local", InetAddress.getLocalHost().getHostName());
+
+        return data;
+    }
+
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        aPort = event.getWebServer().getPort();
+    }
+}
